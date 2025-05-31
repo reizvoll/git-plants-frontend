39bbb9184ffd489babe060c24b88429d1746f0ca
reizvoll <mara2277@naver.com>
2025-05-31 09:41:26 +0900

    feat: add refactored and new components to MainPage (page.tsx)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 import ScrollTopButton from "@/components/shared/ScrollTopButton";
-import BackgroundImage from "./_components/BackgroundImage";
-import MainHero from "./_components/MainHero";
+import HeroSection from "./_components/Section/Hero/HeroSection";
+import UpdateNote from "./_components/Section/Note/UpdateNote";
 
 const MainPage = () => {
   return (
-    <div className="relative -ml-[calc(50vw-50%)] h-[1080px] w-screen">
-      <BackgroundImage />
-      <div className="relative z-10 mx-auto h-full w-full max-w-[1200px] px-16 py-40">
-        <MainHero />
+    <div className="relative -ml-[calc(50vw-50%)] w-screen">
+      <HeroSection />
+      <div className="min-h-screen w-full bg-bg-02">
+        <div className="mx-auto flex w-full max-w-[1200px] flex-col items-start pt-32">
+          <UpdateNote />
+        </div>
       </div>
       <ScrollTopButton />
     </div>
