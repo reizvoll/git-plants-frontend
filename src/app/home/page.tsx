4a5487ef8b505ec1a3aa5476c506da46bbab15d1
reizvoll <mara2277@naver.com>
2025-06-02 13:42:43 +0900

    feat: add ModeSectionCard component with images, apply in page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 import ScrollTopButton from "@/components/shared/ScrollTopButton";
+import ModeSectionCard from "./_components/section/feature/ModeSectionCard";
 import HeroSection from "./_components/section/hero/HeroSection";
 import UpdateNote from "./_components/section/note/UpdateNote";
 
@@ -6,9 +7,10 @@ const MainPage = () => {
   return (
     <div className="relative -ml-[calc(50vw-50%)] w-screen">
       <HeroSection />
-      <div className="min-h-screen w-full bg-bg-02">
-        <div className="mx-auto flex w-full max-w-[1200px] flex-col items-start pt-32">
+      <div className="w-full bg-bg-02">
+        <div className="mx-auto flex w-full max-w-[1200px] flex-col items-start gap-60 pb-20 pt-32">
           <UpdateNote />
+          <ModeSectionCard />
         </div>
       </div>
       <ScrollTopButton />
