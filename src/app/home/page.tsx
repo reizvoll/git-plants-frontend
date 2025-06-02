42642d0fb9b4913f58950006dea5c00bb27f1198
reizvoll <mara2277@naver.com>
2025-06-02 17:18:04 +0900

    feat: install swiper in dependencies, create FeatureSlider Component, and apply it in page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import ScrollTopButton from "@/components/shared/ScrollTopButton";
-import ModeSectionCard from "./_components/section/feature/ModeSectionCard";
+import FeatureSlider from "./_components/section/feature/FeatureSlider";
 import HeroSection from "./_components/section/hero/HeroSection";
 import UpdateNote from "./_components/section/note/UpdateNote";
 
@@ -10,7 +10,7 @@ const MainPage = () => {
       <div className="w-full bg-bg-02">
         <div className="mx-auto flex w-full max-w-[1200px] flex-col items-start gap-60 pb-20 pt-32">
           <UpdateNote />
-          <ModeSectionCard />
+          <FeatureSlider />
         </div>
       </div>
       <ScrollTopButton />
