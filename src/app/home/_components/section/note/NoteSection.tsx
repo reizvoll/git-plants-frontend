46529b4042cb4e208bf6d2b5b41be8513e5ed284
reizvoll <mara2277@naver.com>
2025-06-11 15:07:17 +0900

    feat: update messages and add bilingual support to plants-note

--- src/app/home/_components/section/note/NoteSection.tsx
+++ src/app/home/_components/section/note/NoteSection.tsx
@@ -2,36 +2,32 @@ import corn1 from "@/assets/images/corn1.webp";
 import corn2 from "@/assets/images/corn2.webp";
 import note from "@/assets/images/note.webp";
 import plant from "@/assets/images/plant_icon.png";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 //Todo : refactoring code, and explore strategies for connecting with the back office.
 const NoteSection = () => {
+  const t = useTranslations("plants-note");
   return (
     <div className="flex w-full justify-center">
       <div className="relative h-[634px] w-[1000px]">
         <Image src={note} alt="Note" className="object-cover" />
-        <div className="absolute inset-0 flex items-center justify-center px-12 py-12">
+        <div className="absolute inset-0 flex items-center justify-center px-12 py-20">
           <div className="flex w-full flex-row items-center justify-center gap-20">
             {/* Left Frame - Content Section */}
-            <div className="flex flex-col items-center justify-center gap-8">
+            <div className="flex flex-col items-center justify-center gap-6">
               {/* Section Title */}
-              <div className="flex flex-col items-center gap-4">
+              <div className="flex flex-col items-center gap-6">
                 {/* Heading */}
                 <div className="text-center font-galmuri text-title1 text-primary-strong">
-                  이달의 식물노트:
+                  {t("title")}
                   <br />
-                  옥수수(Corn)
+                  {t("plant")}
                 </div>
 
                 {/* Text */}
-                <div className="text-center font-galmuri text-title2 text-primary-strong">
-                  옥수수는 여름을 대표하는 대표적인 작물로.
-                  <br />
-                  따뜻한 기후에서 잘 자라는 한해살이 식물입니다.
-                  <br />
-                  주로 6월에서 8월 사이에 수확되며
-                  <br />
-                  다양한 용도로 활용됩니다.(팝콘으로 제격이죠.)
+                <div className="whitespace-pre-line text-center font-galmuri text-body2 text-primary-strong">
+                  {t("description")}
                 </div>
               </div>
 
@@ -42,7 +38,7 @@ const NoteSection = () => {
             </div>
 
             {/* Right Frame */}
-            <div className="flex flex-col items-center justify-center gap-20">
+            <div className="flex flex-col items-center justify-center gap-16">
               {/* Main Character Image */}
               <div className="h-[196px] w-[196px] rounded-full bg-bg-01">
                 <Image src={corn1} alt="corn" />
@@ -51,42 +47,36 @@ const NoteSection = () => {
               {/* Content */}
               <div className="flex flex-col items-center gap-4">
                 {/* Row */}
-                <div className="flex flex-row gap-6">
+                <div className="flex flex-row gap-8">
                   {/* First List Item */}
-                  <div className="flex flex-grow flex-col items-start gap-4">
+                  <div className="flex w-auto flex-grow flex-col items-start gap-4">
                     {/* Icon */}
                     <div className="h-[43px] w-12">
                       <Image src={plant} alt="icon" />
                     </div>
 
                     {/* Subheading */}
-                    <div className="font-pretendard text-title2 font-bold text-text-04">성장 단계</div>
+                    <div className="font-pretendard text-title2 font-bold text-text-04">{t("growth_stage.title")}</div>
 
                     {/* Text */}
-                    <p className="font-pretendard text-body1 text-text-04">
-                      씨앗부터 수확까지,
-                      <br />
-                      총 5단계의 성장 과정을
-                      <br />
-                      함께 살펴봐요!
+                    <p className="whitespace-pre-line font-pretendard text-caption text-text-04">
+                      {t("growth_stage.description")}
                     </p>
                   </div>
 
                   {/* Second List Item */}
-                  <div className="flex flex-grow flex-col items-start gap-4">
+                  <div className="flex w-auto flex-grow flex-col items-start gap-4">
                     {/* Icon */}
                     <div className="h-[43px] w-12">
                       <Image src={plant} alt="icon" />
                     </div>
 
                     {/* Subheading */}
-                    <div className="font-pretendard text-title2 font-bold text-text-04">지금 시작해보세요!</div>
+                    <div className="font-pretendard text-title2 font-bold text-text-04">{t("start_now.title")}</div>
 
                     {/* Text */}
-                    <p className="font-pretendard text-body1 text-text-04">
-                      식물을 키우는 즐거움은 물론,
-                      <br />
-                      쏠쏠한 보상도 함께 챙겨봐요!
+                    <p className="whitespace-pre-line font-pretendard text-caption text-text-04">
+                      {t("start_now.description")}
                     </p>
                   </div>
                 </div>
