0da6603e370d7ceb9edf7a17ed8790236736108d
reizvoll <mara2277@naver.com>
2025-07-25 23:06:56 +0900

    feat: add background and pot list components and apply them to client page

--- src/app/shop/_components/ShopPageClient.tsx
+++ src/app/shop/_components/ShopPageClient.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import ScrollTopButton from "@/components/shared/ScrollTopButton";
+import { useShopStore } from "@/lib/store/shopStore";
+import { useEffect } from "react";
 import ShopHero from "./section/hero/ShopHero";
+import BackgroundList from "./section/items/BackgroundList";
+import PotList from "./section/items/PotList";
 import SellCropsSection from "./section/sell-crops/SellCropsSection";
 import UpdateSection from "./section/update/UpdateSection";
 
 const ShopPageClient = () => {
+  const { backgroundItems, potItems, isLoading, fetchShopItems } = useShopStore();
+
+  useEffect(() => {
+    fetchShopItems();
+  }, [fetchShopItems]);
+
   return (
     <>
       <div className="relative w-full bg-sageGreen-100">
@@ -13,6 +23,8 @@ const ShopPageClient = () => {
           <ShopHero />
           <SellCropsSection />
           <UpdateSection />
+          <BackgroundList items={backgroundItems} loading={isLoading} />
+          <PotList items={potItems} loading={isLoading} />
         </div>
       </div>
       <ScrollTopButton />
