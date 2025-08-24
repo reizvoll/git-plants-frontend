54710e554c9abb6ce25dba6b4323cecee9174a0d
reizvoll <mara2277@naver.com>
2025-08-24 13:59:51 +0900

    feat: add error handling and enhance UI/UX in NewUpdatesCard

--- src/app/shop/_components/section/update/NewUpdatesCard.tsx
+++ src/app/shop/_components/section/update/NewUpdatesCard.tsx
@@ -15,12 +15,12 @@ const NewUpdatesCard = ({ isModalOpen }: NewUpdatesCardProps) => {
     return <div>{/* <LoadingSpinner /> */}</div>;
   }
 
-  if (error) {
-    return <div>에러: {error}</div>;
+  {
+    /* 에러처리, 오버레이로 대체 */
   }
 
   return (
-    <div className="mx-auto flex w-full max-w-[800px] flex-col items-center justify-center gap-10 rounded-2xl bg-secondary-light py-[3.75rem]">
+    <div className="mx-auto flex h-[700px] w-full flex-col items-center justify-center gap-10 rounded-2xl px-[60px] py-12 py-[3.75rem]">
       <div className="w-full text-center text-heading text-primary-default">따끈-한 신상 업데이트</div>
 
       <div className="flex w-full flex-col gap-10">
@@ -55,9 +55,42 @@ const NewUpdatesCard = ({ isModalOpen }: NewUpdatesCardProps) => {
             </div>
           </div>
         ) : (
-          <div>추후 업데이트 예정입니다.</div>
+          <>
+            <div className="mx-auto flex h-[360px] w-[700px] items-center justify-center rounded-lg bg-gray-100">
+              <div className="text-center text-body1 text-text-03">추후 업데이트 예정입니다.</div>
+            </div>
+
+            <div className="flex w-full flex-row items-center justify-center gap-10">
+              <Button
+                size="lg"
+                variant="secondary"
+                className="flex items-center justify-center px-[60px] py-4 text-body1 !font-medium"
+                onClick={isModalOpen}
+              >
+                상세보기
+              </Button>
+              <Button
+                size="lg"
+                variant="secondaryLine"
+                className="flex items-center justify-center px-[60px] py-4 text-body1 !font-medium"
+              >
+                사러가기
+              </Button>
+            </div>
+          </>
         )}
       </div>
+
+      {/* error overlay */}
+      {error && (
+        <div className="absolute inset-0 flex items-center justify-center rounded-2xl bg-black/50">
+          <div className="text-center text-subtitle text-text-01">
+            정보를 불러올 수 없습니다.
+            <br />
+            잠시 후 다시 시도해주세요.
+          </div>
+        </div>
+      )}
     </div>
   );
 };
