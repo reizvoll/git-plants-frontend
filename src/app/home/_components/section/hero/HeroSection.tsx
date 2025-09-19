92855a62226fa776d6862cc4fabfed45174d15fd
reizvoll <mara2277@naver.com>
2025-09-19 17:20:20 +0900

    feat: optimize image quality and hero background blur

--- src/app/home/_components/section/hero/HeroSection.tsx
+++ src/app/home/_components/section/hero/HeroSection.tsx
@@ -1,14 +1,18 @@
 import BackgroundImage from "./BackgroundImage";
 import MainHero from "./MainHero";
 
-const HeroSection = () => {
+type HeroSectionProps = {
+  backgroundBlur: string;
+};
+
+const HeroSection = ({ backgroundBlur }: HeroSectionProps) => {
   return (
-    <div className="relative h-[1080px] w-full">
-      <BackgroundImage />
-      <div className="relative z-10 mx-auto h-full w-full max-w-[1200px] px-16 py-40">
+    <section className="relative h-[1080px] w-full">
+      <BackgroundImage blurDataURL={backgroundBlur} />
+      <header className="relative z-10 mx-auto h-full w-full max-w-[1200px] px-16 py-40">
         <MainHero />
-      </div>
-    </div>
+      </header>
+    </section>
   );
 };
 
