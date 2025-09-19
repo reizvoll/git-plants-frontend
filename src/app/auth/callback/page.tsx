1de0e554761d03caad0bb27bb9857cf5406b7e3e
reizvoll <mara2277@naver.com>
2025-09-19 12:19:56 +0900

    refactor: remove wrapper div and apply className props with semantic section in CallbackClient & CallbackPage

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,9 +1,9 @@
 import CallbackClient from "./_components/CallbackClient";
 
-export default function CallbackPage({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
-  return (
-    <div className="flex w-full flex-col items-center justify-center">
-      <CallbackClient error={searchParams.error} />
-    </div>
-  );
+type CallbackPageProps = {
+  searchParams: { [key: string]: string | undefined };
+};
+
+export default function CallbackPage({ searchParams }: CallbackPageProps) {
+  return <CallbackClient error={searchParams.error} className="flex w-full flex-col items-center justify-center" />;
 }
