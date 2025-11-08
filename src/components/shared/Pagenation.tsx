b0b058d46810fb782016847ad80822e792250322
reizvoll <mara2277@naver.com>
2025-11-08 16:13:26 +0900

    refactor: apply constants to pagination component

--- src/components/shared/Pagenation.tsx
+++ src/components/shared/Pagenation.tsx
@@ -2,6 +2,7 @@
 
 import CaretLeft from "@/assets/icons/caret-left.svg";
 import CaretRight from "@/assets/icons/caret-right.svg";
+import { ITEMS_PER_PAGE } from "@/lib/constants/constants";
 import clsx from "clsx";
 
 type PaginationProps = {
@@ -11,7 +12,7 @@ type PaginationProps = {
   limit?: number;
 };
 
-const Pagination = ({ Results, page, handlePageChange, limit = 6 }: PaginationProps) => {
+const Pagination = ({ Results, page, handlePageChange, limit = ITEMS_PER_PAGE.DESKTOP }: PaginationProps) => {
   if (!Results || Results.total === 0) return null;
 
   const totalPages = Math.ceil(Results.total / limit);
@@ -21,13 +22,13 @@ const Pagination = ({ Results, page, handlePageChange, limit = 6 }: PaginationPr
   const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
 
   return (
-    <div className="mt-16 flex items-center justify-center gap-3">
+    <div className="mt-5 flex items-center justify-center gap-1 mb:mt-[60px] mb:gap-3">
       {/* <button
         disabled={page === 1}
         onClick={() => handlePageChange(1)}
         className="flex h-7 w-7 items-center justify-center disabled:text-text-02"
       >
-        <CaretDoubleLeft size={16} weight="bold" className= />
+        <CaretDoubleLeft size={16} weight="bold" className="tb:w-[13px]" />
       </button> */}
 
       <button
@@ -35,20 +36,17 @@ const Pagination = ({ Results, page, handlePageChange, limit = 6 }: PaginationPr
         onClick={() => handlePageChange(page - 1)}
         className="flex h-7 w-7 items-center justify-center disabled:text-text-02"
       >
-        <CaretLeft className="h-4 w-4" strokeWidth={3} />
+        <CaretLeft size={16} weight="bold" className="tb:w-[13px]" />
       </button>
 
       {Array.from({ length: endPage - startPage + 1 }, (_, idx) => startPage + idx).map((pageNumber) => (
         <button
           key={pageNumber}
           onClick={() => handlePageChange(pageNumber)}
-          className={clsx(
-            "text-body flex h-8 w-8 items-center justify-center rounded-full font-pretendard font-medium",
-            {
-              "bg-secondary-default text-text-01": page === pageNumber,
-              "bg-bg-01 text-text-04": page !== pageNumber
-            }
-          )}
+          className={clsx("text-body flex h-8 w-8 items-center justify-center rounded-full font-pretendard", {
+            "bg-secondary-default text-text-01": page === pageNumber,
+            "bg-bg-01 text-text-04": page !== pageNumber
+          })}
         >
           {pageNumber}
         </button>
@@ -59,7 +57,7 @@ const Pagination = ({ Results, page, handlePageChange, limit = 6 }: PaginationPr
         onClick={() => handlePageChange(page + 1)}
         className="flex h-7 w-7 items-center justify-center disabled:text-text-02"
       >
-        <CaretRight className="h-4 w-4" strokeWidth={3} />
+        <CaretRight size={16} weight="bold" className="tb:w-[13px]" />
       </button>
 
       {/* <button
@@ -67,7 +65,7 @@ const Pagination = ({ Results, page, handlePageChange, limit = 6 }: PaginationPr
         onClick={() => handlePageChange(totalPages)}
         className="flex h-7 w-7 items-center justify-center disabled:text-text-02"
       >
-        <CaretDoubleRight size={16} weight="bold"/>
+        <CaretDoubleRight size={16} weight="bold" className="tb:w-[13px]" />
       </button> */}
     </div>
   );
